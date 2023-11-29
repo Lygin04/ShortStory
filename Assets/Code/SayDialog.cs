224b6482b150332276e5e5076cb4f3de575ce701
Lygin04 <kosty.lygin.58@yandex.ru>
2023-11-29 17:59:31 +0300

    bag fix

--- Assets/Code/SayDialog.cs
+++ Assets/Code/SayDialog.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
+using Task = System.Threading.Tasks.Task;
 
 public class SayDialog : MonoBehaviour
 {
@@ -13,7 +10,7 @@ public class SayDialog : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _text;
     
     private int index;
-
+    private bool isWrite;
     private void Awake()
     {
         Dialog();
@@ -24,12 +21,25 @@ public class SayDialog : MonoBehaviour
         if(Input.GetMouseButtonDown(0) || Input.GetKey(KeyCode.Space)) Dialog();
     }
 
-    public void Dialog()
+    private async void Dialog()
     {
-        if(_dialog.Get.Length == index) return;
+        if(_dialog.Get.Length == index || isWrite) return;
 
         _author.text = _dialog.Get[index].Name;
-        _text.text = _dialog.Get[index].Text;
+        isWrite = await WriteDialog(_dialog.Get[index].Text, _text);  
         index++;
+    }
+
+    private async Task<bool> WriteDialog(string text, TextMeshProUGUI meshPro)
+    {
+        isWrite = true;
+        meshPro.text = null;
+        foreach (var value in text)
+        {
+            meshPro.text += value;
+            await Task.Delay(50);
+        }
+
+        return false;
     }
 }
